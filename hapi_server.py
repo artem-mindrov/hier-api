f1a0d5734cf88a2b67187fef9101e231393fecd0
armi0616 <artem.mindrov@netcracker.com>
2018-10-29 18:18:45 +0300

    don't use the TreeSet for simplicity (with a minor perf hit)

--- hapi_server.py
+++ hapi_server.py
@@ -5,83 +5,6 @@ from collections import OrderedDict
 from StringIO import StringIO
 
 
-# https://raw.githubusercontent.com/fukatani/TreeSet/master/treeset.py
-class TreeSet(object):
-    def __init__(self, elements):
-        self._treeset = []
-        self.add_all(elements)
-
-    def add_all(self, elements):
-        for element in elements:
-            if element in self: continue
-            self.add(element)
-
-    def add(self, element):
-        if element not in self:
-            bisect.insort(self._treeset, element)
-
-    def ceiling(self, e):
-        index = bisect.bisect_right(self._treeset, e)
-        if self[index - 1] == e:
-            return e
-        return self._treeset[bisect.bisect_right(self._treeset, e)]
-
-    def floor(self, e):
-        index = bisect.bisect_left(self._treeset, e)
-        if self[index] == e:
-            return e
-        else:
-            return self._treeset[bisect.bisect_left(self._treeset, e) - 1]
-
-    def index(self, e):
-        index = bisect.bisect_left(self._treeset, e)
-        if self[index] == e:
-            return index
-        else:
-            return None
-
-    def __getitem__(self, num):
-        return self._treeset[num]
-
-    def __len__(self):
-        return len(self._treeset)
-
-    def clear(self):
-        self._treeset = []
-
-    def clone(self):
-        return TreeSet(self._treeset)
-
-    def remove(self, element):
-        try:
-            self._treeset.remove(element)
-        except ValueError:
-            return False
-        return True
-
-    def __iter__(self):
-        for element in self._treeset:
-            yield element
-
-    def pop(self, index):
-        return self._treeset.pop(index)
-
-    def __str__(self):
-        return str(self._treeset)
-
-    def __eq__(self, target):
-        if isinstance(target, TreeSet):
-            return self._treeset == target._treeset
-        elif isinstance(target, list):
-            return self._treeset == target
-
-    def __contains__(self, e):
-        try:
-            return e == self._treeset[bisect.bisect_left(self._treeset, e)]
-        except:
-            return False
-
-
 class Node:
     """A tree node identified by a string ID and a name
 
@@ -96,7 +19,7 @@ class Node:
         self._children_by_id = {}  # for more efficient removal
         self._depth = 0
 
-        self.children = TreeSet(elements=[])
+        self.children = []
         self.parent = None
 
     def id(self):
@@ -163,7 +86,8 @@ class Node:
         if not node or node in self.children:
             return False
 
-        self.children.add(node)
+        self.children.append(node)
+        self.children.sort()
         self._children_by_id[node.id()] = node
         node.parent = self
         node.set_depth(self._depth + 1)
